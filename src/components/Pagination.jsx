f8a5ffd511c20bee1a15c990be66d76515055a2f
Danil <danil5-0@mail.ru>
2023-06-30 20:09:18 +0300

    Lesson 5.4

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,6 +1,21 @@
+import _ from 'lodash';
+
 const Pagination = (props) => {
+  const { itemsCount, pageSize} = props;
+  const pageCount = Math.ceil(itemsCount / pageSize);  
+  if (pageCount === 1) return null;
+  const pages = _.range(1, pageCount + 1);
+  console.log(pages)
+
   return (
-    null
+    <nav>
+      <ul className="pagination">
+        {pages.map((page) => (
+          <li className="page-item"><a className="page-link" >{page}</a></li>
+        ))}
+        
+      </ul>
+    </nav>
   );
 }
  
